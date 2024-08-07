6591629e4625c02f96c7a1c794ab4f7cdb91e559
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-07 20:39:00 +0530

    random generation functionality implemented

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
 
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is : {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -15,7 +20,7 @@ fn main() {
 
 // /////////////////////////Processing a Guess /////////////////////////
 
-//The first part of the guessing game program will ask for user input, process that input, and check that the input is in the expected form. To start, we’ll allow the player to input a guess.
+// The first part of the guessing game program will ask for user input, process that input, and check that the input is in the expected form. To start, we’ll allow the player to input a guess.
 
 // Handling potential failure with Result enum, which is a type that represents either success or failure, and is returned by the read_line() function.
 //If we want to handle failure in a program, we can use the Result enum. We’ll use the expect method to convert the error into a string.
@@ -23,3 +28,11 @@ fn main() {
 ////////////////Generating a Secret Number /////////////////////////
 
 //The next part of the guessing game program will generate a secret number. We’ll use the rand crate to generate a random number.
+
+//updating main() function with the secret number 
+
+/* 
+let secret_number = rand::thread_rng().gen_range(1..=100)
+printLn!(The secret number is {}", secret_number)
+
+*/
