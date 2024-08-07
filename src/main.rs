c5ec563247297b84fb57b51b2e6d0b91ce3ee492
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-07 21:22:53 +0530

    handling invalid input functionality implemented

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,27 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is : {}", secret_number);
+    // println!("The secret number is : {}", secret_number);
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+   loop { let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    let guess:u32 = guess.trim().parse().expect("Please type a number");
+    let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
     println!("You guessed: {guess}");
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Equal => {println!("You win!");
+                 break;
+                     }
     }
+}
 }
 
 
@@ -57,7 +63,32 @@ match guess.cmp(&secret_number) {
         Ordering::Equal => println!("You win!"),
 
 We get an error of mismatched types. To overcome this
+
 We create a variable named guess. But wait, doesn’t the program already have a variable named guess? It does, but helpfully Rust allows us to shadow the previous value of guess with a new one. "Shadowing" lets us reuse the guess variable name rather than forcing us to create two unique variables, such as guess_str and guess, for example.
 
+updating the main() function with the following line
+
 let guess: u32 = guess.trim().parse().expect("Please type a number!");
+*/
+
+
+/////////////////////// Allowing Multiple Guesses with Loopin //////////////////
+
+/*
+
+Allowing Multiple Guesses with Looping
+
+The loop keyword creates an infinite loop. We’ll add a loop to give users more chances at guessing the number:
+
+*/
+
+/////////////////////////Quitting after a correct guess /////////////////////////
+/*
+    Adding the break line after You win! makes the program exit the loop when the user guesses the secret number correctly. Exiting the loop also means exiting the program, because the loop is the last part of main.
+*/
+
+/////////////////////////////Handling Invaild Input////////////////////////
+/*
+
+
 */
